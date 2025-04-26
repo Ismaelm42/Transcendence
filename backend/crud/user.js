18b4a32d62c96e4ca99a29a71f5dfb7f1dde9a67
Ismael <imoro-sa@student.42malaga.com>
2025-04-26 11:34:15 +0200

    console.log deleted

--- backend/crud/user.js
+++ backend/crud/user.js
@@ -179,7 +179,6 @@ export const updateLastLogoutById = async (userId) => {
 export const getAllUsersCoincidences = async (userId, keyword) => {
 	try {
 		keyword = String(keyword).toLowerCase();
-		console.log('keyword = ', keyword);
 		const users = await User.findAll({
 			where: {
 				[Op.and]: [
