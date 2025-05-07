14662d88c984d74d00083be65de841cb034b8c08
Ismael <imoro-sa@student.42malaga.com>
2025-05-07 20:44:27 +0200

    websocket keep alive 1hour as token

--- backend/config/websocket.js
+++ backend/config/websocket.js
@@ -3,7 +3,5 @@ import fastifyWebsocket from '@fastify/websocket';
 export function registerWebsocket(fastify) {
 
 	// Register Fastify Websocket
-	console.log('Registering websocket');
 	fastify.register(fastifyWebsocket);
-	console.log('Websocket registered');
 }
