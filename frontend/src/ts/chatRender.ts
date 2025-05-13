7a0cddcee338bb0dcf9d8faf74a0780fe539b07a
fclaus-g <fclaus-g@student.42malaga.com>
2025-05-13 13:51:49 +0200

    feat: add searchInput variable for filter user for keyword

--- frontend/src/ts/chatRender.ts
+++ frontend/src/ts/chatRender.ts
@@ -1,5 +1,5 @@
 import { Step } from './stepRender.js';
-import { handleSocket, handleTextareaKeydown, handleFormSubmit} from './handleChat.js';
+import { handleSocket, handleTextareaKeydown, handleFormSubmit, filterSearchUsers} from './handleChat.js';
 
 export default class Chat extends Step {
 	async render(appElement: HTMLElement): Promise<void> {
@@ -17,6 +17,13 @@ export default class Chat extends Step {
 			const textarea = document.getElementById("chat-textarea") as HTMLTextAreaElement;
 			const chatMessages = document.getElementById("chat-messages") as HTMLDivElement;
 			const items = document.getElementById("item-container") as HTMLDivElement;
+			const searchInput = document.getElementById("search-input") as HTMLInputElement;
+			if (searchInput) {
+				searchInput.addEventListener("input", () => {
+					console.log("Autocomplete search triggered with keyword:", searchInput.value);
+					filterSearchUsers(searchInput.value);
+				});
+			}
 			const stored = sessionStorage.getItem("chatHTML") || "";
 			if (stored) {
 				chatMessages.innerHTML = stored;
@@ -25,6 +32,7 @@ export default class Chat extends Step {
 			const socket = handleSocket(chatMessages, items, this.username? this.username : "Undefined");
 			textarea.addEventListener('keydown', (e) => handleTextareaKeydown(e, form));
 			form.addEventListener('submit', (e) => handleFormSubmit(e, textarea, socket));
+			searchInput.addEventListener('input', () => filterSearchUsers(searchInput.value));
 		}
 		catch (error) {
 				appElement.innerHTML = `<div id="pong-container">An error occurred while generating the content</div>`;
