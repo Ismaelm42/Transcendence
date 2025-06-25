a8584b75b273b50fc94b933b7a4626b4633fc444
Ismael <imoro-sa@student.42malaga.com>
2025-06-25 14:06:22 +0200

    checkifgameisrunnuing added

--- frontend/src/js/chess/state.js
+++
@@ -1,7 +0,0 @@
-let n = 1;
-export function getN() {
-    return n;
-}
-export function changeN(a) {
-    n = a;
-}
--- frontend/src/js/chess/verifySocket.js
+++
@@ -1,5 +0,0 @@
-export function verifySocket(chessSocket) {
-    if (!chessSocket || chessSocket.readyState === WebSocket.CLOSED)
-        chessSocket = new WebSocket("https://localhost:8443/back/ws/chess");
-    return chessSocket;
-}
